cc74ea97fb6857aec7b946433b98e153462fdc52
streino <streino@disroot.org>
2025-12-22 19:30:06 +0100

    more usable mocks

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -2,13 +2,14 @@ import json
 
 from operator import itemgetter
 from pathlib import Path
-from responses import RequestsMock, matchers
+from responses import RequestsMock
+from responses.matchers import header_matcher, json_params_matcher, query_param_matcher
 
 from ecospheres_universe.config import ApiConfig, Config, DeployEnv
 from ecospheres_universe.datagouv import ElementClass
 from ecospheres_universe.feed_universe import feed
 
-from .datagouv_mocks import Dataset, Dataservice, Element, Organization
+from .datagouv_mocks import Dataset, Dataservice, Organization, Topic
 
 
 def json_load_path(path: Path):
@@ -16,28 +17,28 @@ def json_load_path(path: Path):
         return json.load(f)
 
 
+def mock_organizations_file(organizations: list[Organization]) -> list[dict[str, str]]:
+    return sorted(
+        [{"id": org.id, "name": org.name, "slug": org.slug, "type": ""} for org in organizations],
+        key=itemgetter("name"),
+    )
+
+
 def test_full_run(responses: RequestsMock, tmp_path: Path):
     datagouv_url = "https://www.example.com/datagouv"
     grist_url = "https://www.example.com/grist"
     fake_token = "fake-token"
 
-    datasets = Dataset.some(3)
-    dataservices = Dataservice.some(2)
+    organizations = Organization.some(2)
 
-    existing_universe = [
-        Element(datasets[0]),
-        Element(datasets[1]),
-        Element(dataservices[0]),
-    ]
+    datasets = Dataset.some_owned(3, organizations)
+    dataservices = Dataservice.some_owned(2, organizations)
 
-    target_universe = [
-        Organization(
-            objects={ElementClass.Dataset: [datasets[0], datasets[2]]},
-        ),
-        Organization(
-            objects={ElementClass.Dataservice: [dataservices[0], dataservices[1]]},
-        ),
-    ]
+    existing_universe = Topic(datasets[0], datasets[1], dataservices[0])
+
+    target_universe = (
+        existing_universe.clone().add(datasets[2], dataservices[1]).remove(datasets[1])
+    )
 
     conf = Config(
         env=DeployEnv.DEMO,
@@ -51,16 +52,17 @@ def test_full_run(responses: RequestsMock, tmp_path: Path):
     # grist.get_organizations()
     _ = responses.get(
         url=grist_url,
-        match=[
-            matchers.query_param_matcher(
-                {"filter": json.dumps({"env": [conf.env.value]}), "limit": 0}
-            )
-        ],
-        json={"records": [{"fields": {"slug": org.slug, "type": ""}} for org in target_universe]},
+        match=[query_param_matcher({"filter": json.dumps({"env": [conf.env.value]}), "limit": 0})],
+        json={
+            "records": [
+                {"fields": {"slug": org.slug, "type": ""}}
+                for org in target_universe.organizations()
+            ]
+        },
     )
 
     # datagouv.get_organization()
-    for org in target_universe:
+    for org in target_universe.organizations():
         _ = responses.get(
             url=f"{datagouv_url}/api/1/organizations/{org.slug}/",
             json={"id": org.id, "slug": org.slug, "name": org.name},
@@ -69,18 +71,18 @@ def test_full_run(responses: RequestsMock, tmp_path: Path):
     # reset=False by default => skip datagouv.delete_all_topic_elements()
 
     for element_class in ElementClass:
-        existing_elements = [
-            elem for elem in existing_universe if elem.element_class is element_class
-        ]
-        target_ids = [obj.id for org in target_universe for obj in org.objects(element_class)]
+        existing_elements = existing_universe.elements(element_class)
+        target_elements = target_universe.elements(element_class)
 
         # datagouv.get_organization_objects_ids()
-        for org in target_universe:
+        for org in target_universe.organizations():
             _ = responses.get(
                 url=f"{datagouv_url}/api/2/{element_class.value}/search/",
-                match=[matchers.query_param_matcher({"organization": org.id}, strict_match=False)],
+                match=[query_param_matcher({"organization": org.id}, strict_match=False)],
                 json={
-                    "data": [{"id": obj_id} for obj_id in target_ids],
+                    "data": [
+                        {"id": e.object.id} for e in target_universe.elements(element_class, org)
+                    ],
                     "next_page": None,
                 },
             )
@@ -88,51 +90,34 @@ def test_full_run(responses: RequestsMock, tmp_path: Path):
         # datagouv.get_topic_elements()
         _ = responses.get(
             url=f"{datagouv_url}/api/2/topics/{conf.topic}/elements/",
-            match=[
-                matchers.query_param_matcher(
-                    {"class": element_class.name},
-                    strict_match=False,
-                )
-            ],
+            match=[query_param_matcher({"class": element_class.name}, strict_match=False)],
             json={
                 "data": [
-                    {
-                        "id": elem.id,
-                        "element": {"class": elem.element_class.name, "id": elem.element_id},
-                    }
-                    for elem in existing_elements
+                    {"id": e.id, "element": {"class": element_class.name, "id": e.object.id}}
+                    for e in existing_elements
                 ],
                 "next_page": None,
             },
         )
 
         # datagouv.put_topic_elements()
-        additions = [
-            obj_id
-            for obj_id in target_ids
-            if obj_id not in {elem.element_id for elem in existing_elements}
-        ]
+        additions = [e.object.id for e in target_elements if e not in existing_elements]
         _ = responses.post(
             url=f"{datagouv_url}/api/2/topics/{conf.topic}/elements/",
             match=[
-                matchers.header_matcher(
-                    {"Content-Type": "application/json", "X-API-KEY": fake_token}
-                ),
-                matchers.json_params_matcher(
-                    [
-                        {"element": {"class": element_class.name, "id": obj_id}}
-                        for obj_id in additions
-                    ]
+                header_matcher({"Content-Type": "application/json", "X-API-KEY": fake_token}),
+                json_params_matcher(
+                    [{"element": {"class": element_class.name, "id": oid}} for oid in additions]
                 ),
             ],
         )
 
         # datagouv.delete_topic_elements()
-        removals = [elem.id for elem in existing_elements if elem.element_id not in target_ids]
-        for elem_id in removals:
+        removals = [e.id for e in existing_elements if e not in target_elements]
+        for eid in removals:
             _ = responses.delete(
-                url=f"{datagouv_url}/api/2/topics/{conf.topic}/elements/{elem_id}/",
-                match=[matchers.header_matcher({"X-API-KEY": fake_token})],
+                url=f"{datagouv_url}/api/2/topics/{conf.topic}/elements/{eid}/",
+                match=[header_matcher({"X-API-KEY": fake_token})],
             )
 
     # datagouv.get_bouquets()
@@ -140,26 +125,19 @@ def test_full_run(responses: RequestsMock, tmp_path: Path):
     _ = responses.get(
         url=f"{datagouv_url}/api/2/topics/",
         match=[
-            matchers.header_matcher({"X-API-KEY": fake_token}),
-            matchers.query_param_matcher(
-                {
-                    "tag": conf.tag,
-                    "include_private": "yes",
-                }
-            ),
+            header_matcher({"X-API-KEY": fake_token}),
+            query_param_matcher({"tag": conf.tag, "include_private": "yes"}),
         ],
         json={"data": [], "next_page": None},
     )
 
     feed(conf)
 
-    # FIXME: correct subset of orgs
-    assert json_load_path(tmp_path / f"organizations-datasets-{conf.env.value}.json") == sorted(
-        [{"id": org.id, "name": org.name, "slug": org.slug, "type": ""} for org in target_universe],
-        key=itemgetter("name"),
-    )
-    assert json_load_path(tmp_path / f"organizations-dataservices-{conf.env.value}.json") == sorted(
-        [{"id": org.id, "name": org.name, "slug": org.slug, "type": ""} for org in target_universe],
-        key=itemgetter("name"),
-    )
+    for element_class in ElementClass:
+        actual = json_load_path(
+            tmp_path / f"organizations-{element_class.value}-{conf.env.value}.json"
+        )
+        expected = mock_organizations_file(target_universe.organizations(element_class))
+        assert actual == expected
+
     assert json_load_path(tmp_path / f"organizations-bouquets-{conf.env.value}.json") == []
