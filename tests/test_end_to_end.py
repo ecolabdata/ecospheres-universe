371c8f87415c817448ac6461555a71f46a1b9866
streino <streino@disroot.org>
2026-01-06 17:16:24 +0100

    compact tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -7,12 +7,10 @@ from .datagouv_mocks import Dataset, Dataservice, Organization, Topic
 
 def test_all_at_once(run_mock_feed: Callable, feed_config: Config):
     organizations = Organization.some(5)
-
     datasets = Dataset.some_owned(3, organizations)
     dataservices = Dataservice.some_owned(2, list(reversed(organizations)))
 
     existing_universe = Topic(datasets[0], datasets[1], dataservices[0])
-
     target_universe = (
         existing_universe.clone()
         .add_elements(datasets[2], dataservices[1])
@@ -26,12 +24,10 @@ def test_all_at_once(run_mock_feed: Callable, feed_config: Config):
 
 def test_no_changes(run_mock_feed: Callable, feed_config: Config):
     organizations = Organization.some(5)
-
     datasets = Dataset.some_owned(3, organizations)
     dataservices = Dataservice.some_owned(2, list(reversed(organizations)))
 
     existing_universe = Topic(*datasets, *dataservices)
-
     target_universe = existing_universe.clone()
 
     run_mock_feed(feed_config, existing_universe, target_universe, bouquets=None)
@@ -39,12 +35,10 @@ def test_no_changes(run_mock_feed: Callable, feed_config: Config):
 
 def test_bootstrap_universe(run_mock_feed: Callable, feed_config: Config):
     organizations = Organization.some(5)
-
     datasets = Dataset.some_owned(3, organizations)
     dataservices = Dataservice.some_owned(2, list(reversed(organizations)))
 
     existing_universe = Topic()
-
     target_universe = existing_universe.clone().add_elements(*datasets, *dataservices)
 
     run_mock_feed(feed_config, existing_universe, target_universe, bouquets=None)
@@ -52,23 +46,20 @@ def test_bootstrap_universe(run_mock_feed: Callable, feed_config: Config):
 
 def test_remove_everything(run_mock_feed: Callable, feed_config: Config):
     organizations = Organization.some(5)
-
     datasets = Dataset.some_owned(3, organizations)
     dataservices = Dataservice.some_owned(2, list(reversed(organizations)))
 
     existing_universe = Topic(*datasets, *dataservices)
-
     target_universe = existing_universe.clone().remove_elements(*datasets, *dataservices)
 
     run_mock_feed(feed_config, existing_universe, target_universe, bouquets=None)
 
 
 def test_bouquets_orgs(run_mock_feed: Callable, feed_config: Config):
-    organizations = Organization.some(5)
-
     existing_universe = Topic()
     target_universe = existing_universe.clone()
 
-    bouquets = Topic.some_owned(len(organizations), [org.with_type(None) for org in organizations])
+    organizations = [org.with_type(None) for org in Organization.some(5)]
+    bouquets = Topic.some_owned(len(organizations), organizations)
 
     run_mock_feed(feed_config, existing_universe, target_universe, bouquets)
