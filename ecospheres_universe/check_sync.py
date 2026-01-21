462c02ec21c4b04ae0729e4653a6b42ae9e8157c
streino <streino@disroot.org>
2026-01-21 16:12:17 +0100

    fix table config

--- ecospheres_universe/check_sync.py
+++ ecospheres_universe/check_sync.py
@@ -30,7 +30,7 @@ def check_sync(universe: Path, *extra_configs: Path):
 
     grist = GristApi(
         base_url=conf.grist.url,
-        table=conf.grist.url,
+        table=conf.grist.table,
         token=os.getenv("GRIST_API_KEY", conf.grist.token),
     )
 
